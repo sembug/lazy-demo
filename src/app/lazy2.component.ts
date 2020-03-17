320ab79956cda213a8f564f29ed1715ab2d1db1c
Roberto Nunes <roberto@krooze.com.br>
2020-03-17 14:57:45 -0300

    Add lazy components

---
+++ src/app/lazy2.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  template: `
+    <p>
+      lazy2 works!
+    </p>
+  `,
+  styles: []
+})
+export class Lazy2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
